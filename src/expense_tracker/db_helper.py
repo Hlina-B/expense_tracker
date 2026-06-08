3952c1bf8c99ea17524361b3613ceb2523593313
Hlina Tibebu <beyenehlina@gmail.com>
2026-06-08 14:18:07 -0400

    Delete unwanted file in wrong directory

--- src/expense_tracker/db_helper.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-def get_path(file_name: str) -> Path:
-    current_directory = Path(__file__).parent.parent
-    file_path = current_directory / "storage" / file_name
-    file_path.parent.mkdir(parents=True, exist_ok=True)
-    return file_path
